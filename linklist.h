cebd52cca9fea510a6dffd673ffef14afe392ea8
xianweihuihuan <2907404984@qq.com>
2024-11-16 22:31:01 +0800

    c++实现链表——头文件

---
+++ linklist.h
@@ -0,0 +1,30 @@
+#pragma once
+#include<iostream>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+#include<assert.h>
+
+struct node{
+    int val;
+    node*next;
+};
+
+class linklist{
+protected:
+    node*head;
+    node*tail;
+public:
+    linklist(){
+        head = nullptr;
+        tail = nullptr;
+    }
+    node* buynode(int n);
+    void pushback(int n );
+    void pushhead(int n );  
+    void deletback();
+    void delethead();
+    int finddata(int n );
+    void print();
+
+};
